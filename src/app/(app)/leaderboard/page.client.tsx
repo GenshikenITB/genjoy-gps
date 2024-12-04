b442fde87b302335182f8f523b06a79e457f520f
yoshikazuuu <febrianojerry@gmail.com>
2024-12-04 11:49:24 +0700

    feat: get all users score

--- src/app/(app)/leaderboard/page.client.tsx
+++ src/app/(app)/leaderboard/page.client.tsx
@@ -1,8 +1,44 @@
+"use client";
+
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { motion, AnimatePresence } from "framer-motion";
+import { LoaderCircle } from "lucide-react";
+import { api } from "@/trpc/react";
+
 export function LeaderboardClientPage() {
+  const users = api.mamet.getAllEnrollments.useQuery();
+  console.log(users);
+
   return (
-    <div>
-      <h1>Leaderboard</h1>
-      <p>Leaderboard page content</p>
-    </div>
+    <Card className="bg-secondary">
+      <CardHeader className="p-4">
+        <motion.span
+          className="font-black"
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          ADD SIDE QUEST
+        </motion.span>
+      </CardHeader>
+      <AnimatePresence mode="wait">
+        <CardContent className="px-4">
+          {users.isLoading && (
+            <motion.div
+              key="loading"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <Card>
+                <CardHeader className="flex items-center justify-center">
+                  <LoaderCircle className="h-8 w-8 animate-spin text-primary" />
+                </CardHeader>
+              </Card>
+            </motion.div>
+          )}
+        </CardContent>
+      </AnimatePresence>
+    </Card>
   );
 }
--- src/server/api/routers/mamet.ts
+++ src/server/api/routers/mamet.ts
@@ -1,121 +1,156 @@
+import { calculatePoints } from "@/lib/score";
 import { sendMessage } from "@/lib/webhook";
-import {
-    createTRPCRouter,
-    protectedProcedure,
-} from "@/server/api/trpc";
+import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
 import { questFormSchema } from "@/validations/quest";
 import { z } from "zod";
 
 export const mametRouter = createTRPCRouter({
-    getAllEnrollments: protectedProcedure
-        .query(async ({ ctx }) => {
-            // Get all enrollments
-            return await ctx.db.questEnrollment.findMany({
-                include: {
-                    quest: true,
-                    user: true,
-                }
-            });
-        }),
+  getAllEnrollments: protectedProcedure.query(async ({ ctx }) => {
+    // Get all enrollments
+    return await ctx.db.questEnrollment.findMany({
+      include: {
+        quest: true,
+        user: true,
+      },
+    });
+  }),
 
-    unapproveActiveParticipate: protectedProcedure
-        .input(z.string())
-        .mutation(async ({ ctx, input: id }) => {
-            // Unapprove a user's presence
-            return await ctx.db.questEnrollment.update({
-                where: {
-                    id: id,
-                },
-                data: {
-                    isActivelyParticipating: false,
-                },
-            });
-        }),
+  unapproveActiveParticipate: protectedProcedure
+    .input(z.string())
+    .mutation(async ({ ctx, input: id }) => {
+      // Unapprove a user's presence
+      return await ctx.db.questEnrollment.update({
+        where: {
+          id: id,
+        },
+        data: {
+          isActivelyParticipating: false,
+        },
+      });
+    }),
 
-    approveActiveParticipate: protectedProcedure
-        .input(z.string())
-        .mutation(async ({ ctx, input: id }) => {
-            // Approve a user's presence
-            return await ctx.db.questEnrollment.update({
-                where: {
-                    id: id,
-                },
-                data: {
-                    isActivelyParticipating: true,
-                },
-            });
-        }),
+  approveActiveParticipate: protectedProcedure
+    .input(z.string())
+    .mutation(async ({ ctx, input: id }) => {
+      // Approve a user's presence
+      return await ctx.db.questEnrollment.update({
+        where: {
+          id: id,
+        },
+        data: {
+          isActivelyParticipating: true,
+        },
+      });
+    }),
 
-    verifyPresence: protectedProcedure
-        .input(z.string())
-        .mutation(async ({ ctx, input: id }) => {
-            // Verify a user's presence
-            return await ctx.db.questEnrollment.update({
-                where: {
-                    id: id,
-                },
-                data: {
-                    isPresentVerified: true,
-                },
-            });
-        }),
+  verifyPresence: protectedProcedure
+    .input(z.string())
+    .mutation(async ({ ctx, input: id }) => {
+      // Verify a user's presence
+      return await ctx.db.questEnrollment.update({
+        where: {
+          id: id,
+        },
+        data: {
+          isPresentVerified: true,
+        },
+      });
+    }),
 
-    unverifyPresence: protectedProcedure
-        .input(z.string())
-        .mutation(async ({ ctx, input: id }) => {
-            // Unverify a user's presence
-            return await ctx.db.questEnrollment.update({
-                where: {
-                    id: id,
-                },
-                data: {
-                    isPresentVerified: false,
-                },
-            });
-        }),
+  unverifyPresence: protectedProcedure
+    .input(z.string())
+    .mutation(async ({ ctx, input: id }) => {
+      // Unverify a user's presence
+      return await ctx.db.questEnrollment.update({
+        where: {
+          id: id,
+        },
+        data: {
+          isPresentVerified: false,
+        },
+      });
+    }),
 
-    createQuest: protectedProcedure
-        .input(questFormSchema)
-        .mutation(async ({ ctx, input }) => {
-            // Create a new quest
-            const quest = await ctx.db.quest.create({
-                data: input,
-            });
+  createQuest: protectedProcedure
+    .input(questFormSchema)
+    .mutation(async ({ ctx, input }) => {
+      // Create a new quest
+      const quest = await ctx.db.quest.create({
+        data: input,
+      });
 
-            await sendMessage(quest);
+      await sendMessage(quest);
 
-            return quest;
-        }),
+      return quest;
+    }),
 
-    editQuest: protectedProcedure
-        .input(z.object({
-            id: z.string(),
-            data: questFormSchema,
-        }))
-        .mutation(async ({ ctx, input }) => {
-            // Edit a quest
-            return await ctx.db.quest.update({
-                where: {
-                    id: input.id,
-                },
-                data: input.data,
-            });
-        }),
+  editQuest: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        data: questFormSchema,
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      // Edit a quest
+      return await ctx.db.quest.update({
+        where: {
+          id: input.id,
+        },
+        data: input.data,
+      });
+    }),
 
-    deleteQuest: protectedProcedure
-        .input(z.string())
-        .mutation(async ({ ctx, input: id }) => {
-            // Delete a quest
-            return await ctx.db.quest.delete({
-                where: {
-                    id: id,
-                },
-            });
-        }),
+  deleteQuest: protectedProcedure
+    .input(z.string())
+    .mutation(async ({ ctx, input: id }) => {
+      // Delete a quest
+      return await ctx.db.quest.delete({
+        where: {
+          id: id,
+        },
+      });
+    }),
 
-    getQuests: protectedProcedure
-        .query(async ({ ctx }) => {
-            // Get all quests
-            return await ctx.db.quest.findMany();
-        }),
-})
+  getQuests: protectedProcedure.query(async ({ ctx }) => {
+    // Get all quests
+    return await ctx.db.quest.findMany();
+  }),
+
+  getAllUsersScore: protectedProcedure.query(async ({ ctx }) => {
+    // Get all users' scores
+    const users = await ctx.db.user.findMany({
+      include: {
+        QuestEnrollment: {
+          where: {
+            completedAt: { not: null },
+            isPresent: { not: null },
+          },
+          include: {
+            quest: true,
+          },
+        },
+      },
+    });
+
+    const userScores = users.map((user) => ({
+      userId: user.id,
+      name: user.name,
+      image: user.image,
+      score: user.QuestEnrollment.reduce(
+        (acc, questEnrollment) =>
+          acc +
+          calculatePoints(
+            questEnrollment.quest.type,
+            questEnrollment.quest.isHandsOn,
+            questEnrollment.isActivelyParticipating,
+            questEnrollment.isPresentVerified,
+            questEnrollment.isPresent,
+          ),
+        0,
+      ),
+    }));
+
+    return userScores.sort((a, b) => b.score - a.score);
+  }),
+});
